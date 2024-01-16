eed810d9dfdde2702ea8bd72fd97595d99a9b445
shejialuo <shejialuo@gmail.com>
2024-01-16 21:05:27 +0800

    feat: Finish homework 1

--- src/Homework1/Ans.hs
+++ src/Homework1/Ans.hs
@@ -1,12 +1,43 @@
 module Homework1.Ans
   ( toDigits,
+    toDigitsRev,
+    doubleEveryOther,
+    sumDigits,
+    validate,
+    hanoi,
   )
 where
 
 toDigits :: Integer -> [Integer]
-toDigits x
+toDigits = reverse . toDigitsRev
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x
   | x <= 0 = []
-  | otherwise = value : toDigits nextX
+  | otherwise = value : toDigitsRev nextX
   where
     nextX = div x 10
     value = mod x 10
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther l@(x : xs)
+  | even $ length l = (x * 2) : doubleEveryOther xs
+  | otherwise = x : doubleEveryOther xs
+doubleEveryOther [] = []
+
+sumDigits :: [Integer] -> Integer
+sumDigits = foldr ((+) . sum . toDigitsRev) 0
+
+validate :: Integer -> Bool
+validate cardNumber = (sumDigits . doubleEveryOther . toDigits) cardNumber `mod` 10 == 0
+
+type Peg = String
+
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 source target _ = [(source, target)]
+hanoi n source target auxiliary =
+  hanoi (n - 1) source auxiliary target
+    ++ [(source, target)]
+    ++ hanoi (n - 1) auxiliary target source
--- test/Homework1/Test.hs
+++ test/Homework1/Test.hs
@@ -1,15 +1,63 @@
 module Homework1.Test (testSuite) where
 
-import Homework1.Ans (toDigits)
+import Homework1.Ans (doubleEveryOther, hanoi, sumDigits, toDigits, toDigitsRev, validate)
 import Test.HUnit
 
-testToDigits1 :: Test
-testToDigits1 = TestCase (assertEqual "toDigits 0 should be []" [] $ toDigits 0)
+testToDigits :: Test
+testToDigits =
+  TestList
+    [ TestCase $ do
+        assertEqual "toDigits 0" [] $ toDigits 0
+        assertEqual "toDigits -17" [] $ toDigits (-11)
+        assertEqual "toDigits 1" [1] $ toDigits 1
+        assertEqual "toDigits 1234" [1, 2, 3, 4] $ toDigits 1234
+    ]
 
-testToDigits2 :: Test
-testToDigits2 = TestCase (assertEqual "toDigits -17 should be []" [] $ toDigits (-17))
+testToDigitsRev :: Test
+testToDigitsRev =
+  TestList
+    [ TestCase $ do
+        assertEqual "toDigits 0" [] $ toDigitsRev 0
+        assertEqual "toDigits -17" [] $ toDigitsRev (-11)
+        assertEqual "toDigits 1" [1] $ toDigitsRev 1
+        assertEqual "toDigitsRev 1234" [4, 3, 2, 1] $ toDigitsRev 1234
+    ]
+
+testDoubleEveryOther :: Test
+testDoubleEveryOther =
+  TestList
+    [ TestCase $ do
+        assertEqual "doubleEveryOther [8, 7, 6, 5]" [16, 7, 12, 5] $ doubleEveryOther [8, 7, 6, 5]
+        assertEqual "doubleEveryOther [1, 2, 3]" [1, 4, 3] $ doubleEveryOther [1, 2, 3]
+    ]
+
+testSumDigits :: Test
+testSumDigits =
+  TestList
+    [ TestCase $ do
+        assertEqual "sumDigits [16,7,12,5]" 22 $ sumDigits [16, 7, 12, 5]
+    ]
+
+testValidate :: Test
+testValidate =
+  TestList
+    [ TestCase $ do
+        assertEqual "validate 4012888888881881" True $ validate 4012888888881881
+        assertEqual "validate 4012888888881882" False $ validate 4012888888881882
+    ]
+
+testHanoi :: Test
+testHanoi =
+  TestList
+    [ TestCase $ do
+        assertEqual "hanoi 2" [("a", "c"), ("a", "b"), ("c", "b")] $ hanoi 2 "a" "b" "c"
+        assertEqual
+          "hanoi 3"
+          [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")]
+          $ hanoi 3 "a" "b" "c"
+    ]
 
 testSuite :: Test
 testSuite =
   TestList
-    [testToDigits1, testToDigits2]
+    [testToDigits, testToDigitsRev, testDoubleEveryOther, testSumDigits, testValidate, testHanoi]
